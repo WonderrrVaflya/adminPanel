8b0b71e9c318045cd27dff0010e3d90af7a661a7
Egor <mak95mak@gmail.com>
2024-06-27 12:04:07 +0300

    final

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import StorePage from './pages/StorePage';
-import SearchPage from './pages/SearchPage';
 import { Col, Row, Tabs } from 'antd';
 
 const App: React.FC = () => {
@@ -17,7 +16,6 @@ const App: React.FC = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/stores/:id" element={<StorePage />} />
-            <Route path="/search" element={<SearchPage />} />
           </Routes>
         </Col>
       </Row>
